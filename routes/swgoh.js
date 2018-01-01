6d8cf5e84071df267799fb263c071212f68a85a3
William Pruden <williamprudeniv@gmail.com>
2018-01-01 14:43:24 -0800

    update routes

--- routes/swgoh.js
+++ routes/swgoh.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var swgohController = require("../controllers/swgoh-controller.js")
 
+//http://localhost:3000/api/v1/swgoh
 router.get('/characters', swgohController.characters);
 router.get('/ships', swgohController.ships);
 router.get('/profile', swgohController.profile);
--- routes/utility.js
+++ routes/utility.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var utilityController = require("../controllers/utility-controller.js")
 
+//http://localhost:3000/api/v1/utility
 router.get('/character-url-list', utilityController.characterUrlList);
 router.get('/character-master', utilityController.characterMaster);
 router.get('/ship-url-list', utilityController.shipUrlList);
